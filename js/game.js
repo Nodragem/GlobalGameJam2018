61a939f46cea9017145934be66c1d06c7e969291
Chris Munasinha <chrism@crispymix.com>
2018-01-27 13:06:25 +0000

    Map loading!

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+var map;
+var layer;
 var Game = {
     // I played with the variables scopes:
     // these variables don't need to be exposed!
@@ -7,19 +9,41 @@ var Game = {
     Flowers: [],
 
     preload : function() {
-        // Here we load all the needed resources for the level.
-        // In our case, that's just two squares - one for the snake body and one for the apple.
+        game.stage.backgroundColor = '#1f8ec1';
         game.load.image('hive', './assets/images/hive_ph.png');
         game.load.image('bee', './assets/images/bee_ph.png');
         game.load.image('love_bee', './assets/images/love_bee_ph.png');
         game.load.image('seed', './assets/images/seed_ph.png');
         game.load.image('flower', './assets/images/flower_ph.png');
+        game.load.tilemap('level-1', '/assets/maps/level-1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles', 'assets/images/tilemap.png');
+
     },
 
     create : function() {
-        this.Flowers.push(new Flower(20, 20));
-        this.Bees.push(new Bee(200, 200));
-        this.Hives.push(new Hive(500, 500));
+        //this.Flowers.push(new Flower(20, 20));
+        //this.Bees.push(new Bee(200, 200));
+        //this.Hives.push(new Hive(500, 500));
+
+        map = game.add.tilemap('level-1');
+        map.addTilesetImage('level-objects', 'tiles', 60, 60, 1, 1);
+
+        layer = map.createLayer('world');
+        for(var y = 0; y < map.height; ++y){
+            for(var x = 0; x < map.width; ++x){
+                var tile = map.getTile(x, y);
+                if(tile) {
+                    var object_type = tile.properties['type'];
+                    switch(object_type) {
+                        case 'flower':
+                            this.Flowers.push(new Flower(x*60,y*60));
+                        break;
+                    }
+                }
+            }
+        }
+
+
     },
 
     update: function() {
---
+++ js/level-load.js
@@ -0,0 +1,24 @@
+var LoadLevel = {
+
+
+  preload : function() {
+  },
+
+  create : function() {
+
+  },
+
+  update: function() {
+
+  },
+
+  endGame: function () {
+
+    // Change the state back to Game.
+
+  }
+
+};
+
+var map;
+var layer;
